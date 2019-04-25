0e29e5f567b6c29cc5429b928162ae67c37b5255
Julio Lopez <julioj.lopeza@gmail.com>
2019-04-25 00:32:13 -0400

    API call with redux thunk

--- src/thunk.js
+++ src/thunk.js
@@ -1,6 +1,36 @@
+const FETCH_START = 'start'
+const FETCH_SUCCESS = 'success'
+const FETCH_ERROR = 'error'
+
+const fetchStart = () => ({
+    type: FETCH_START,
+})
+
+const fetchSuccess = payload => ({
+    type: FETCH_START,
+    payload
+})
+
+const fetchError = error => ({
+    type: FETCH_START,
+    error
+})
+
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+
 export default payload =>
-(dispatch, getState) => {
-    console.log(payload)
-    const state = getState()
-    console.log(state)
+async (dispatch, getState) => {
+    dispatch(fetchStart())
+    try {
+        const result = await fetch(url)
+        const json = await result.json()
+        dispatch(fetchSuccess(json))
+        console.log(json)
+        
+    } catch (error) {
+        dispatch(fetchError(error))
+        
+    }
+     
 }
